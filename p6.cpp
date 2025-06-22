8f74dc8f6a85be748ffdc1d0e68951eab6fda0a2
sn12eh34 <167397035+sn12eh34@users.noreply.github.com>
2025-06-22 21:11:53 +0530

    Update p6.cpp

--- p6.cpp
+++ p6.cpp
@@ -1,3 +1,28 @@
+
+/* Problem Statement: Given an integer N, print the following pattern : 
+
+
+Here, N = 5.
+
+Examples:
+
+Input Format: N = 3
+Result: 
+1 2 3
+1 2
+1
+
+Input Format: N = 6
+Result:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 

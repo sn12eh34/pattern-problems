c466d1dab36f1c9a52e48c66ace1ff3196be0192
sn12eh34 <167397035+sn12eh34@users.noreply.github.com>
2025-06-22 21:08:44 +0530

    Update p22.cpp

---
+++ p22.cpp
@@ -0,0 +1,47 @@
+/* Problem Statement: Given an integer N, print the following pattern : 
+
+
+Here, N = 4.
+
+Examples:
+
+Input Format: N = 3
+Result: 
+3 3 3 3 3 
+3 2 2 2 3 
+3 2 1 2 3 
+3 2 2 2 3 
+3 3 3 3 3
+
+Input Format: N = 6
+Result:   
+6 6 6 6 6 6 6 6 6 6 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 2 1 2 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 6 6 6 6 6 6 6 6 6 6 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int size = 2 * n - 1;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int min_dist = min(min(i, j), min(size - 1 - i, size - 1 - j));
+            cout << n - min_dist << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
